4d892ae47b6d31f2b73d3099b73145780e868eeb
joachimmp3 <joachim.mahfouf@phosforea.com>
2025-04-06 14:29:18 +0200

    More deployment config

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const config = require('./webpack.config');
+const path = require('path')
+const { merge } = require('webpack-merge')
+const config = require('./webpack.config')
 
 module.exports = merge(config, {
   mode: 'production',
@@ -8,4 +8,4 @@ module.exports = merge(config, {
   output: {
     path: path.resolve(__dirname, 'public'),
   },
-});
+})
